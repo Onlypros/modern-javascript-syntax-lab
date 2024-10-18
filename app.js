fb6a604f46d3ffc290a06c5db6a4c5cf9d968818
Onlyuser <jrlumpkinjr@gmail.com>
2024-10-18 14:20:45 -0400

    finished

---
+++ app.js
@@ -0,0 +1,157 @@
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+// Your code here
+const nums2 = nums.map((numElement) => {
+    return numElement *2
+});
+
+console.log('exercise 1:', nums2);
+
+// array destructuring
+const petsArray = ['Rover', 'Snuffles', 'Toto', 'Spot'];
+
+const [firstPet, secondPet] = petsArray;
+
+console.log(firstPet); // 'Rover'
+console.log(secondPet); // 'Snuffles'
+
+// Equivalent in traditional bracket notation:
+console.log(petsArray[0]); // 'Rover'
+console.log(petsArray[1]); // 'Snuffles'
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const [firstTopping, secondTopping] = pizzaToppings;
+
+console.log('exercise 2:', pizzaToppings[0]);
+console.log('exercise 2:', pizzaToppings[1]);
+
+// object destructing
+const person = {
+    name: 'Alex',
+    role: 'Software Engineer',
+  };
+  
+  // Object destructuring:
+  const { name, role } = person;
+  
+  console.log(name); // 'Alex'
+  console.log(role); // 'Software Engineer'
+  
+  // Equivalent in traditional dot notation:
+  console.log(person.name); // 'Alex'
+  console.log(person.role); // 'Software Engineer'
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Your code here
+const {make, model} = car;
+
+console.log('exercise 3 make:', make);
+console.log('exercsie 3 model:', model);
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const controversialPizzaToppings = [...pizzaToppings]
+
+console.log('exercise 4 controversialPizzaToppings:', controversialPizzaToppings)
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+
+// const car = {
+//   make: 'Audi',
+//   model: 'q5',
+// };
+
+// Change the `model` property of `myCar` to 'q7'. Log both objects.
+
+// Your code here
+myCar = {...car}
+myCar.model = 'q7'
+
+console.log('exercise 5 myCar:', myCar)
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+const userProfile = {}
+const propertyName = 'username'
+
+userProfile[propertyName] = 'helloworld'
+
+console.log('exercise 6 userProfile:', userProfile)
+
+console.log('exercise 7: completed')
+
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+function exericse8(noun='cat', adjective='white') {
+  console.log(`exercise8: The ${noun} is ${adjective}.`)
+}
+
+exericse8();
+
+// Convert the following `if...else` statement in to a ternary:
+
+let pizza = 'tasty';
+
+if (pizza === 'tasty') {
+  console.log('yum');
+} else {
+  console.log('yuck');
+}
+
+// Your code here
+pizza === 'tasty' ? console.log('exercise 9: yum') : console.log('yuck')
+pizza === 'nasty' ? console.log('yum') : console.log('exercise 9: yuck')
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+const LANG = localLangConfig || 'en'
+// Log the result
+console.log('exercise 10: Language setting:', LANG);
+
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+  name: 'Alice',
+};
+
+let cat = adventurer.cat?.name;
+
+console.log('exercise 11:', cat);
+
